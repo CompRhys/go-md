626f5ca5191638259d7aa68f5392c81b17461f2e
CompRhys <reag2@cam.ac.uk>
2019-03-02 09:30:36 +0000

    Update timestep.go

--- core/timestep.go
+++ core/timestep.go
@@ -7,7 +7,7 @@ import (
 
 // TimeStep evolves the system by one unit of time using the Velocity Verlet algorithm
 // for molecular dynamics using channels to provide simple parallelisarion.
-func TimeStep(R, V []r3.Vector, L, M, h float64) ([]r3.Vector, []r3.Vector) {
+func TimeStepVV(R, V []r3.Vector, L, M, h float64) ([]r3.Vector, []r3.Vector) {
 	N := len(R)
 	A := make([]r3.Vector, N)
 	nR := make([]r3.Vector, N)
@@ -33,7 +33,7 @@ func TimeStep(R, V []r3.Vector, L, M, h float64) ([]r3.Vector, []r3.Vector) {
 }
 
 // WarmUpStep as above but with force capping to prevent the system exploding
-func WarmUpStep(R, V []r3.Vector, L, M, F_max, h float64) ([]r3.Vector, []r3.Vector) {
+func WarmUpStepVV(R, V []r3.Vector, L, M, F_max, h float64) ([]r3.Vector, []r3.Vector) {
 	N := len(R)
 	A := make([]r3.Vector, N)
 	nR := make([]r3.Vector, N)
@@ -60,6 +60,34 @@ func WarmUpStep(R, V []r3.Vector, L, M, F_max, h float64) ([]r3.Vector, []r3.Vec
 		if nF_mag > F_max {
 			nFi.Mul(F_max/nF_mag)
 		}
+		nAi := nFi.Mul(1.0/M)
+		nV[i] = integrators.NextV(V[i], A[i], nAi, h)
+	}
+	return nR, nV
+}
+
+
+// TimeStep evolves the system by one unit of time using the langevin thermostat
+// for molecular dynamics using channels to provide simple parallelisarion.
+func TimeStepBD(R, V []r3.Vector, L, M, h float64) ([]r3.Vector, []r3.Vector) {
+	N := len(R)
+	A := make([]r3.Vector, N)
+	nR := make([]r3.Vector, N)
+	nV := make([]r3.Vector, N)
+	c := make(chan ForceReturn, N)
+	for i := 0; i < N; i++ { go InternalForce(i, R, L, c) }
+	for n := 0; n < N; n++ {
+		info := <-c
+		i := info.i
+		Fi := info.F
+		A[i] = Fi.Mul(1.0/M)
+		nR[i] = PutInBox(integrators.NextR(R[i], V[i], A[i], h), L)
+	}
+	for i := 0; i < N; i++ { go InternalForce(i, nR, L, c) }
+	for n := 0; n < N; n++ {
+		info := <-c
+		i := info.i
+		nFi := info.F
 		nAi := nFi.Mul(1.0/M)
 		nV[i] = integrators.NextV(V[i], A[i], nAi, h)
 	}
